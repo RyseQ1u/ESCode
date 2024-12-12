3bbdfa9f93985873570b4e82612202b82f3e92a1
RyseQ1u <460255780@qq.com>
2024-12-13 00:43:33 +0800

    14  15  16

---
+++ js/model.js
@@ -0,0 +1,12 @@
+let name = 'boy'
+let gender = 'gg'
+
+let getAge = () =>{
+    return  19
+}
+
+//export{name,gender,getAge}
+//使用 default 导出成对象
+// export default {name,gender,getAge}
+//* as 获取成对象
+export  {name,gender,getAge}
